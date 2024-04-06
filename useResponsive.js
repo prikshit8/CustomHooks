beca17aa79e9e5b08c42b80844397494346db53e
prikshit8 <prikshitchawla.v@gmail.com>
2024-04-06 16:04:31 +0530

    useResponsive hook

---
+++ useResponsive.js
@@ -0,0 +1,29 @@
+import { useState } from "react";
+
+export const useResponsive = () => {
+  const [device, setDevice] = useState({
+    mobile: false,
+    tablet: false,
+    computer: false,
+  });
+
+  const deviceCheck = () => {
+    const width = window.innerWidth;
+    const mobile = width <= 768;
+    const tablet = width >= 768 && width <= 990;
+    const computer = width >= 990;
+    setDevice({ mobile, tablet, computer });
+  };
+  useState(() => {
+    // first calculation
+    deviceCheck();
+
+    window.addEventListener("resize", deviceCheck);
+
+    // cleanup
+    return () => {
+      window.removeEventListener("resize", deviceCheck);
+    };
+  }, []);
+  return device;
+};
